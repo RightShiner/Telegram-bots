095866965f6e38bec31fedb0a3e603fe720bc48e
RightShiner <brilight.shiner@gmail.com>
2023-03-14 18:07:40 +0100

    Update Telemetry.js

--- Telemetry - TelegramBot 3/Telemetry.js
+++ Telemetry - TelegramBot 3/Telemetry.js
@@ -1,79 +1,27 @@
-//This code sets up a Telegram bot that allows users to set maximum and minimum volume alarms for a certain cryptocurrency pair using the CoinMarketCap API. 
-//When a user sets an alarm, the bot stores the maximum and minimum volume for that pair in an object called volumeAlarms. 
-//The bot then checks the volume for each cryptocurrency pair.
+//The bot then checks the volume for each dex pair every hour and send notification if the daily volumen is out of range. 
 
 const { Telegraf } = require('telegraf')
 
 const bot = new Telegraf('<YOUR_BOT_TOKEN>')
 
-// Register a pool
-bot.command('registerpool', async (ctx) => {
-  // Prompt the user for the pool details
-  await ctx.reply('Please provide the pool name:')
-  const poolName = await ctx.scene.enter('poolName')
+//thepgraph Api key
 
-  await ctx.reply('Please provide the DEX pool address:')
-  const poolAddress = await ctx.scene.enter('poolAddress')
+//Pool1 (name, pools address, router addreess, blockchain, min volumen ($), max volumen ($)
 
-  await ctx.reply('Please choose a router:', {
-    reply_markup: {
-      inline_keyboard: [
-        [
-          { text: 'Uniswap v2', callback_data: 'uniswapv2' },
-          { text: 'Uniswap v3', callback_data: 'uniswapv3' }
-        ],
-        [
-          { text: 'Pancakeswap', callback_data: 'pancakeswap' }
-        ]
-      ]
-    }
-  })
+//pool2 (name, pools address, router addreess, blockchain, min volumen ($), max volumen ($)
 
-  // Handle the user's router selection
-  bot.action(['uniswapv2', 'uniswapv3', 'pancakeswap'], (ctx) => {
-    const router = ctx.callbackQuery.data
 
-    // Prompt the user to choose a blockchain
-    ctx.reply('Please choose a blockchain:', {
-      reply_markup: {
-        inline_keyboard: [
-          [
-            { text: 'Ethereum', callback_data: 'ethereum' },
-            { text: 'Binance Smart Chain', callback_data: 'bsc' }
-          ],
-          [
-            { text: 'Polygon', callback_data: 'polygon' },
-            { text: 'Celo', callback_data: 'celo' }
-          ]
-        ]
-      }
-    })
+/// The graph queries
+//
+//
+//
+//
+//
+//
+//
+//
 
-    // Handle the user's blockchain selection
-    bot.action(['ethereum', 'bsc', 'polygon', 'celo'], (ctx) => {
-      const blockchain = ctx.callbackQuery.data
-
-      // Save the pool details to a database or file
-      savePool(poolName, poolAddress, router, blockchain)
-
-      ctx.reply(`Pool ${poolName} has been registered for ${blockchain} on ${router}.`)
-    })
-  })
-})
-
-// Set the minimum and maximum volume range
-bot.command('setvolumerange', async (ctx) => {
-  // Prompt the user for the range details
-  await ctx.reply('Please provide the minimum volume:')
-  const minVolume = await ctx.scene.enter('minVolume')
-
-  await ctx.reply('Please provide the maximum volume:')
-  const maxVolume = await ctx.scene.enter('maxVolume')
-
-  // Save the range to a database or file
-  saveVolumeRange(minVolume, maxVolume)
-
-  ctx.reply(`Volume range has been set to ${minVolume} - ${maxVolume}.`)
-})
 
 bot.launch()
+
+// notification here
