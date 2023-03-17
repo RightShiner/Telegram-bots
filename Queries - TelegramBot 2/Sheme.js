ca9735a0d8d34a86fc1c2b6ddad87a95bc6e3018
RightShiner <brilight.shiner@gmail.com>
2023-03-17 20:43:27 +0100

    Create Sheme.js

---
+++ Queries - TelegramBot 2/Sheme.js
@@ -0,0 +1,33 @@
+//This is just an example to show the basic structure we are looking for to query the outputs from the bitquery script.
+
+const { Telegraf } = require('telegraf')
+//Axios
+const bot = new Telegraf('<YOUR_BOT_TOKEN>')
+//thepgraph Api key
+
+ClientVariables:
+
+Wallet cluster:
+0xE90377F95cd3744bd501B3505C7420Ea6dA12e3A, 0xEA93e807d832bAE0560485A37d9bA98D52D152c7, 0xB0F0A7C4Cd59d79638730D9f88Ce728BCFC3977a, 0x6c6c6949477b9B701772cFd828597f7DD1eE7476
+Pool Name: Xcre/USDC 
+Exchange: UniSwap v3 
+Blockchain: Polygon
+Pool: 0x32c936874ff276c626a9ed028faa10071e3a2fac
+
+/// The graph query
+//
+//
+//
+//
+//
+//
+//
+//
+
+
+bot.launch()
+
+//Comand 1 - input start date
+//command 2 - input end date
+//start querying - await
+//show results
