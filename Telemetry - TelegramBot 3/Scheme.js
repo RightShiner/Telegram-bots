4e5a83576542f307a06a3623b8362a15509c75f6
RightShiner <brilight.shiner@gmail.com>
2023-03-22 23:54:00 +0100

    feat: get 24h Volume data from dexscreener

---
+++ Telemetry - TelegramBot 3/Scheme.js
@@ -0,0 +1,36 @@
+const pairs = [
+    {
+        Name: "Xcre/USDC",
+        Exchange: "UniSwap v3",
+        Blockchain: "Polygon",
+        Pool: "0x32c936874ff276c626a9ed028faa10071e3a2fac",
+        Min: 1000,
+        Max: 3000
+    },
+    {
+        Name: "Ethix/Eth",
+        Exchange: "UniSwap v2",
+        Blockchain: "Ethereum",
+        Pool: "0xb14b9464b52f502b0edf51ba3a529bc63706b458",
+        Min: 1000,
+        Max: 10000
+    },
+    {
+        Name: "Ethix/Cusd",
+        Exchange: "UniSwap v3",
+        Blockchain: "Celo",
+        Pool: "0x3420720e561f3082f1e514a4545f0f2e0c955a5d",
+        Min: 10000,
+        Max: 20000
+    },
+    {
+        Name: "BBCN/BNB",
+        Exchange: "PancakeSwap",
+        Blockchain: "BSC",
+        Pool: "0x0883147a16d0ccaab5554c140a3435c74b202c66",
+        Min: 100,
+        Max: 2000
+    },
+]
+
+module.exports = pairs;
