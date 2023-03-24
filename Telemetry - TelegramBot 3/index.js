f75ba352f055937b4beeb647e3f18922cfa75a96
RightShiner <brilight.shiner@gmail.com>
2023-03-24 01:08:33 +0100

    fix: fix show data order

--- Telemetry - TelegramBot 3/index.js
+++ Telemetry - TelegramBot 3/index.js
@@ -28,6 +28,8 @@ let chain = '';
 let h24 = 0;
 let h1 = 0;
 let vol_range_flag = 0;
+let vol_tmp = [];
+let vol_len = 0;
 
 bot.command('start', async (ctx) => {
   await ctx.reply('Bot started.');
@@ -51,19 +53,22 @@ var task = cron.schedule('0 */3 * * *', () => {
 });
 
 const pair_show = () => {
+  vol_len = 0;
   pairs.forEach(async (value, index) => {
-    bot.telegram.sendMessage(process.env.CHAI_ID,
+    vol_tmp[index] = 
       `🤑Pair${index + 1}\n` +
       `Name: ${value.Name}\n` +
       `Exchange: ${value.Exchange}\n` +
       `Blockchain: ${value.Blockchain}\n` +
       `Pool: ${value.Pool}\n` +
-      `Volumen range: $${value.Min}-${value.Max}/day\n`
-    );
+      `Volumen range: $${value.Min}-${value.Max}/day\n`;
+      vol_len++;
   });
+  show_message(vol_len);
 }
 const vol_show = (data_flag) => {
   vol_range_flag = 0;
+  vol_len = 0;
   pairs.forEach(async (value, index) => {
     switch (value.Blockchain) {
       case 'Polygon':
@@ -89,15 +94,16 @@ const vol_show = (data_flag) => {
         h24 = response.data.pair.volume.h24;
         h1 = response.data.pair.volume.h1;
         if (data_flag == 1) {
-          bot.telegram.sendMessage(process.env.CHAI_ID,
+          vol_tmp[index] =
             `🤑Pair${index + 1}\n` +
             `Name: ${value.Name}\n` +
             `Exchange: ${value.Exchange}\n` +
             `Blockchain: ${value.Blockchain}\n` +
             `Pool: ${value.Pool}\n` +
             `Volumen range: $${value.Min}-${value.Max}/day\n` +
-            `dailyVolumeData: ${h24}`
-          );
+            `dailyVolumeData: ${h24}`;
+          vol_len++;
+          show_message(vol_len);
         }
         if (data_flag == 2) {
           if (h24 > value.Max || h24 < value.Min) {
@@ -108,8 +114,7 @@ const vol_show = (data_flag) => {
               `✅Blockchain: ${value.Blockchain}\n` +
               `✅Pool: ${value.Pool}\n\n` +
               `❎Volumen range: $${value.Min}-${value.Max}/day\n` +
-              `❗dailyVolumeData is out range value: ${h24}`
-            );
+              `❗dailyVolumeData is out range value: ${h24}`);
           }
           else {
             vol_range_flag++;
@@ -126,4 +131,13 @@ const vol_show = (data_flag) => {
       });
   });
 }
+
+const show_message = () => {
+  if (vol_len == pairs.length) {
+    bot.telegram.sendMessage(process.env.CHAI_ID, vol_tmp.join('\n\n'));
+    vol_tmp = [];
+    vol_len = 0;
+  }
+}
+
 bot.startPolling();
